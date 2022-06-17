b2d7036bc5bee4fdeaa74fbf30e9a4d77f7aad58
IasArchi <iasmimcardoso11@gmail.com>
2022-06-17 18:05:33 -0300

    adicionando uma chave valor para agilizar a busca do numero do quarto do cliente

--- src/br/dell/modelos/Principal.java
+++ src/br/dell/modelos/Principal.java
@@ -1,7 +1,9 @@
 package br.dell.modelos;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Scanner;
 
 
 public class Principal {
@@ -25,51 +27,60 @@ public class Principal {
                 validadoCpf = true;
             } catch (InputMismatchException exception) {
                 System.err.println("Erro: Digite o numero de CPF");
-            }scanner.nextLine();
-        }
-            System.out.println("Informe o RG do hospede");
-            rgHospede = scanner.nextLong();
-            while (!validaIdade) {
-                try {
-                    System.out.println("Digite a idade do hospede");
-                    idadeHospede = scanner.nextInt();
-                    validaIdade = true;
-
-                } catch (InputMismatchException exception) {
-                    System.err.println("Erro: digite um numero");
-                }scanner.nextLine();
-
             }
+            scanner.nextLine();
+        }
+        System.out.println("Informe o RG do hospede");
+        rgHospede = scanner.nextLong();
+        while (!validaIdade) {
+            try {
+                System.out.println("Digite a idade do hospede");
+                idadeHospede = scanner.nextInt();
+                validaIdade = true;
 
-            System.out.println("Digite o endereço do hospede");
-            enderecoHospede = scanner.nextLine();
+            } catch (InputMismatchException exception) {
+                System.err.println("Erro: digite um numero");
+            }
+            scanner.nextLine();
+
+        }
+
+        System.out.println("Digite o endereço do hospede");
+        enderecoHospede = scanner.nextLine();
+
+        Map<Integer, String> hospedeQuarto = new HashMap<Integer, String>();
+        System.out.println("Informe o nome do hospede");
+        nomeHospede = scanner.nextLine();
+        System.out.println("Digite o numero do quarto");
+        numeroDoQuarto = scanner.nextInt();
+
+        hospedeQuarto.put(numeroDoQuarto,nomeHospede);
 
 
-            System.out.println("Informe o nome do hospede");
-            nomeHospede = scanner.nextLine();
+        Quarto quarto = new Quarto(numeroDoQuarto, hospede);
 
+        hospede.setNome(nomeHospede);
+        hospede.setCPF(cpfHospede);
+        hospede.setRG(rgHospede);
+        hospede.setIdade(idadeHospede);
+        hospede.setEndereço(enderecoHospede);
 
-            System.out.println("Digite o numero do quarto");
-            numeroDoQuarto = scanner.nextInt();
+        System.out.println("Hospede cadastrado com sucesso");
 
-            Quarto quarto = new Quarto(numeroDoQuarto, hospede);
+        System.out.println(" O numero do quarto é " + quarto.getNumero() + " " + quarto.getHospede().toString());
 
-            hospede.setNome(nomeHospede);
-            hospede.setCPF(cpfHospede);
-            hospede.setRG(rgHospede);
-            hospede.setIdade(idadeHospede);
-            hospede.setEndereço(enderecoHospede);
-
-            System.out.println("Hospede cadastrado com sucesso");
-
-            System.out.println(" O numero do quarto é " + quarto.getNumero() + " " + quarto.getHospede().toString());
+        System.out.println("Digite o nome para busca: ");
+        String nomeBusca=scanner.nextLine();
+        String numeroQuartoBuscado = hospedeQuarto.get(nomeBusca);
+        System.out.println( " O numero do quarto do cliente "+nomeHospede +" é " + numeroDoQuarto);
 
         validacao(nomeHospede);
-        }
+    }
 
     public static boolean validacao(String nomeHospede) {
-        boolean validar = (nomeHospede.matches("\\[A-Z\\]\\[a-z\\]"));
+        boolean validar = (nomeHospede.matches("\\[A-Z]\\[a-z]"));
         System.out.println(" O nome de hospede (" +nomeHospede +") é válido? : " + validar);
         return validar;
     }
+
 }
