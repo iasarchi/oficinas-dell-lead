273767f00609d4b29fcc75defc3a97de244f6fe5
IasArchi <iasmimcardoso11@gmail.com>
2022-07-14 11:37:16 -0300

    Desenvolvendo um sistema de vendas de produtos digitais, para o curso immersion promgram 2 da lead dell

--- src/Produto/Produto.java
+++ src/Produto/Produto.java
@@ -1,5 +1,39 @@
 package Produto;
 
 public class Produto {
-    String nome;
+    private String nome;
+    private double preco;
+    private String path;
+
+    public Produto(String nome, double preco, String path) {
+        this.nome = nome;
+        this.preco = preco;
+        this.path = path;
+    }
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
